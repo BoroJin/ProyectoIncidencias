14e3b77366973c57de14a6d77167eede465c7195
elizabeth <hr.elizabeth.27@gmail.com>
2024-11-11 05:06:37 -0300

    corrección de error ortográfico

--- frontend/src/views/Resolutor_view.jsx
+++ frontend/src/views/Resolutor_view.jsx
@@ -5,7 +5,7 @@ const ResolutorView = () => {
       <div>
         <div className="d-flex">
           <main className="p-4 flex-grow-1">
-            {/* Aquí puedes añadir el contenido específico para la vista de Gestor Territorial */}
+            {/* Aquí puedes añadir el contenido específico para la vista de Resolutor */}
             <h1>Bienvenido, Resolutor</h1>
             <p>Esta es la vista principal para el Resolutor.</p>
           </main>
