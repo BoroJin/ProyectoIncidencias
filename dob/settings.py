2160cd049eef9ff2003fb1025fc4cf682d759b48
Boris <boris.puentes@cloud.uautonoma.cl>
2024-11-15 22:04:14 -0300

    Correcion del requirmentes.txt

--- dob/settings.py
+++ dob/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'urban_sensor',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'alo123',
     }
 }
 
