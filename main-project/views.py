9697d6664754bb56396d5bc9a1fdcbc74c1048d3
BenjaminMorales <benpido@gmail.com>
2024-11-30 20:22:26 -0300

    Fix para DepartamentoObras

--- main-project/views.py
+++ main-project/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+
 def login(request):
     return render(request, 'login/build/index.html')
 
 def departamentoObra(request):
-    return render(request, 'departamentoObras/dist/index.html')
+    return render(request, 'DepartamentoObras/dist/index.html')
