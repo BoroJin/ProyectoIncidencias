994bd68ceb5feb5374874130959d95a0733785fe
maximiliano <maximiliano.brionesv@gmail.com>
2024-12-03 13:15:26 -0300

    sdsdsd

--- ticket/src/componentes/manuales.js
+++ ticket/src/componentes/manuales.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const ManualUsuario = () => {
   const descargarPDFadmin = () => {
@@ -9,11 +8,27 @@ const ManualUsuario = () => {
     enlace.download = "ManualAdministrador.pdf";
     enlace.click();
   };
+  const descargarPDFdepa = () => {
+    const rutaPDF = "/manuales/manualdepartamento.pdf"; // Ruta desde public
+    const enlace = document.createElement("a");
+    enlace.href = rutaPDF;
+    enlace.download = "ManualDepartamento.pdf";
+    enlace.click();
+  };
+
   const descargarPDFdirector = () => {
     const rutaPDF = "/manuales/manualdirector.pdf"; // Ruta desde public
     const enlace = document.createElement("a");
     enlace.href = rutaPDF;
     enlace.download = "ManualDirector.pdf";
+    enlace.click();
+  };
+
+  const descargarPDFresolutor = () => {
+    const rutaPDF = "/manuales/manualresolutor.pdf"; // Ruta desde public
+    const enlace = document.createElement("a");
+    enlace.href = rutaPDF;
+    enlace.download = "ManualResolutor.pdf";
     enlace.click();
   };
   return (
@@ -39,8 +54,8 @@ const ManualUsuario = () => {
                   etc.
                 </li>
                 <li className="listaItem">
-                  Director de obras: El director es el encargado de gestionar
-                  los equipos de trabajo de la aplicación. El será el
+                  Departamento de obras: El departamento es el encargado de
+                  gestionar los equipos de trabajo de la aplicación. El será el
                   responsable de designar al respectivo equipo resolutor
                   capacitado para cada incidencia.
                 </li>
@@ -56,6 +71,11 @@ const ManualUsuario = () => {
                   definir si es que el cierre corresponde o si aún falta para
                   darla por finalizada.
                 </li>
+                <li className="listaItem">
+                  Director: Director es aquel que crea los formularios
+                  dinámicos, para su posterior uso por los gestores. Tambien
+                  puede visualizar las incidencias y rechazarlas.
+                </li>
               </ul>
             </div>
           </div>
@@ -63,14 +83,17 @@ const ManualUsuario = () => {
             <button className="btn" onClick={descargarPDFadmin}>
               Descargar Manual de Administrador
             </button>
-            <button className="btn" onClick={descargarPDFdirector}>
-              Descargar Manual de Director de Obras
+            <button className="btn" onClick={descargarPDFdepa}>
+              Descargar Manual de Departamento de obras
             </button>
             <button className="btn" onClick={descargarPDFadmin}>
               Descargar Manual de Gestor Territorial
             </button>
-            <button className="btn" onClick={descargarPDFadmin}>
+            <button className="btn" onClick={descargarPDFresolutor}>
               Descargar Manual de Resolutor
+            </button>
+            <button className="btn" onClick={descargarPDFdirector}>
+              Descargar Manual de Director
             </button>
           </div>
         </div>
